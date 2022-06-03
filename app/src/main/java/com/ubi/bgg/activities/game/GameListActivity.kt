ea4d17b995955b6f6fe007a18034a452a00325f4
Hououin Kyouma <inwazzin@gmail.com>
2022-06-03 22:54:29 +0200

    add game list + game rank list stubs

--- app/src/main/java/com/ubi/bgg/activities/game/GameListActivity.kt
+++ app/src/main/java/com/ubi/bgg/activities/game/GameListActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ubi.bgg.Common
 import com.ubi.bgg.R
+import com.ubi.bgg.databinding.ActivityGameListBinding
 
 class GameListActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityGameListBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     Common.initialize(applicationContext)
+    binding = ActivityGameListBinding.inflate(layoutInflater)
   }
 }
--- app/src/main/java/com/ubi/bgg/activities/game/GameRankListActivity.kt
+++ app/src/main/java/com/ubi/bgg/activities/game/GameRankListActivity.kt
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ubi.bgg.Common
 import com.ubi.bgg.R
+import com.ubi.bgg.databinding.ActivityRankListBinding
 
-class GameExtensionListActivity : AppCompatActivity() {
+class GameRankListActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityRankListBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     Common.initialize(applicationContext)
+    binding = ActivityRankListBinding.inflate(layoutInflater)
   }
 }
